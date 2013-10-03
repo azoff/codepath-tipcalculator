ab277c1ee8cd900ad8e9d00420b14668cf62166f
Jonathan Azoff <jon@azoffdesign.com>
2013-10-03 00:46:18 -0700

    Step one: get it working

--- TipCalculator/src/main/java/com/example/tipcalculator/CalculatorActivity.java
+++ TipCalculator/src/main/java/com/example/tipcalculator/CalculatorActivity.java
@@ -2,22 +2,84 @@ package com.example.tipcalculator;
 
 import android.os.Bundle;
 import android.app.Activity;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.util.Log;
+import android.view.KeyEvent;
 import android.view.Menu;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.ToggleButton;
+
+import java.text.NumberFormat;
+import java.util.ArrayList;
+import java.util.logging.Logger;
 
 public class CalculatorActivity extends Activity {
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_calculator);
-    }
+	double tip;
+	double price;
 
+	TextView tvResult;
+	EditText etPrice;
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.calculator, menu);
-        return true;
-    }
-    
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_calculator);
+		tvResult = (TextView) findViewById(R.id.tvResult);
+		etPrice = (EditText) findViewById(R.id.etPrice);
+		etPrice.setOnKeyListener(new PriceListener());
+	}
+
+	public void pickTip(View tbSelected) {
+
+		// set the tip
+		CharSequence tipChars = ((ToggleButton)tbSelected).getText();
+		if (tipChars != null) {
+			String decimal = String.format("0.%s", tipChars.subSequence(0,2));
+			tip = Double.valueOf(decimal);
+		}
+
+		// only one toggle can be on
+		ViewGroup tbParent = (ViewGroup) tbSelected.getParent();
+		if (tbParent != null) {
+			int index = tbParent.getChildCount();
+			while(index-- > -1) {
+				ToggleButton tb = (ToggleButton) tbParent.getChildAt(index);
+				if (tb != null) tb.setChecked(tbSelected == tb);
+			}
+		}
+
+		render();
+
+	}
+
+	class PriceListener implements View.OnKeyListener {
+		public boolean onKey(View view, int i, KeyEvent keyEvent) {
+			Editable priceText = etPrice.getText();
+			if (priceText == null) price = 0;
+			else if ("".equals(priceText.toString())) price = 0;
+			else price = Double.valueOf(priceText.toString());
+			return render();
+		}
+	}
+
+	public boolean render() {
+
+		String result = "";
+
+		if (tip > 0 && price > 0) {
+			NumberFormat formatter = NumberFormat.getCurrencyInstance();
+			result = formatter.format(price * tip);
+		}
+
+		tvResult.setText(result);
+
+		return false;
+
+	}
+
 }
